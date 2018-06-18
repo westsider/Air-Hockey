dae71c37b146a355cbb2dd5331e92507c94ae604
Warren Hansen <whansen1@me.com>
2018-06-18 09:17:38 -0500

    task: added gravity, pan gestures

--- Air Hockey/ViewController.swift
+++ Air Hockey/ViewController.swift
@@ -25,10 +25,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomPaddle: UIView!
     
     @IBOutlet weak var puck: UIView!
+
+    var annimator:UIDynamicAnimator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        annimator = UIDynamicAnimator(referenceView: view)
+        
+//        let gravityBehavior = UIGravityBehavior(items: [puck])
+//        gravityBehavior.gravityDirection = CGVector(dx: 0, dy: -1 )
+//        annimator.addBehavior(gravityBehavior)
+        
+        let collisionBehavior = UICollisionBehavior(items: [puck, topPaddle, bottomPaddle])
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        annimator.addBehavior(collisionBehavior)
     }
+    
+    @IBAction func userPannedTopHalf(_ sender: UIPanGestureRecognizer) {
+    }
+    
+    @IBAction func userPannedBottomHalf(_ sender: UIPanGestureRecognizer) {
+    }
+    
 
 
 }
